b67d3fb940142808ee782a4651f970707791005c
Shcherbakov Alexandr <34583499+cheetos32@users.noreply.github.com>
2019-01-24 16:36:40 +0300

    Добавил  spinner
    Добавил spinner, связал, подправил дизайн

--- app/src/main/java/com/hfad/findbeer101/MainActivity.java
+++ app/src/main/java/com/hfad/findbeer101/MainActivity.java
@@ -12,27 +12,35 @@ import java.util.List;
 
 public class MainActivity extends Activity {
     private PoiskPiva robot = new PoiskPiva();
+    private TextView rezultat;
+    private Spinner cvetPiva;
+    private Spinner proizvodstvo;
+    private Button poiskPiva;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.rezultat = (TextView) findViewById(R.id.idArray);
+        this.cvetPiva = (Spinner) findViewById(R.id.idSpinner);
+        this.proizvodstvo = (Spinner) findViewById(R.id.idSpinner2);
+        this.poiskPiva = (Button) findViewById(R.id.idButton);
     }
 
     public void onClickPoisk(View view) {
-        TextView rezultat = (TextView) findViewById(R.id.idArray);
-        Spinner cvetPiva = (Spinner) findViewById(R.id.idSpinner);
-        Button poiskPiva = (Button) findViewById(R.id.idButton);
+        
         String vibranniyCvetPiva = String.valueOf(cvetPiva.getSelectedItem());
+        String vibrannayaStrana = String.valueOf(proizvodstvo.getSelectedItem());
 
-        List <String> rezultatPiva = robot.getCvetPiva(vibranniyCvetPiva);
-        StringBuilder rezultatkonechniy = new StringBuilder();
+
+        List <String> rezultatPiva = robot.getMark(vibranniyCvetPiva,vibrannayaStrana);
+        StringBuilder vivodMarok = new StringBuilder();
         for (String brand: rezultatPiva) {
-            rezultatkonechniy.append(brand).append('\n');
+            vivodMarok.append(brand).append('\n');
 
         }
 
-        rezultat.setText(rezultatkonechniy);
+        rezultat.setText(vivodMarok);
 
 
     }
--- app/src/main/java/com/hfad/findbeer101/PoiskPiva.java
+++ app/src/main/java/com/hfad/findbeer101/PoiskPiva.java
@@ -6,16 +6,60 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PoiskPiva {
-    List <String> getCvetPiva (String cvet) {
+    List <String> getMark (String cvet,String strana) {
         List <String> rezultat = new ArrayList<>();
-        if (cvet.equals("Светлое")) {
+        if (cvet.equals("Светлое")&strana.equals("Россия")) {
             rezultat.add("Жигулёвское");
         }
+        else if (cvet.equals("Светлое")) {
+
+            rezultat.add("Miller");
+            rezultat.add("Gosser");
+            rezultat.add("Korona");
+            rezultat.add("Колос");
+            rezultat.add("Три медведя");
+            rezultat.add("Heineken");
+            rezultat.add("Халзан");
+            rezultat.add("Amstel");
+            rezultat.add("Охота");
+            rezultat.add("Zlaty Bazant");
+            rezultat.add("Lowenbrau");
+            rezultat.add("Faxe");
+            rezultat.add("Bavaria");
+            rezultat.add("Krucovice Imperial");
+            rezultat.add("Stella Artois");
+            rezultat.add("Балтика");
+            rezultat.add("Krombacher");
+            rezultat.add("Leffe Blonde");
+
+        }
         else if (cvet.equals("Тёмное")){
-            rezultat.add("Велкопоповицкий козел");
+            rezultat.add("Velkopopovicky Kozel Cerny");
+            rezultat.add("Tuborg Black");
+            rezultat.add("Krusovice Cerne");
+            rezultat.add("Guinness Original");
+            rezultat.add("Paulaner Hefe-Weissbier");
+            rezultat.add("Genevieve deBrabant Double");
+            rezultat.add("Grimbergen Double-Ambree");
+            rezultat.add("Belhaven Black Scottish Stout");
+            rezultat.add("AndechsWeissbier Dunkel");
+            rezultat.add("Пензенское Премиум");
+
+
+
+
+
+
         }
         else if (cvet.equals("Янтарное")) {
             rezultat.add("Ярпиво");
+            rezultat.add("Хамовники Пшеничное");
+            rezultat.add("Menabrea La 150° Ambrata");
+            rezultat.add("Alpkonig Weitnauer Marzen");
+            rezultat.add("Bruegel Amber Ale");
+
+
+
         }
      return rezultat;}
 
