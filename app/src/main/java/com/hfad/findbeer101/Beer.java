da1b8b008d57036c8833f04811d5236dc11e31f2
Shcherbakov Alexandr <34583499+cheetos32@users.noreply.github.com>
2019-01-25 20:54:17 +0300

    новые классы beer и marks

---
+++ app/src/main/java/com/hfad/findbeer101/Beer.java
@@ -0,0 +1,25 @@
+package com.hfad.findbeer101;
+
+public class Beer {
+    String mark;//название марки пива
+    String cvet; // цвет пива
+    String strana; // страна производства
+    //String krepost; // крепость напитка: лёгкое, среднее, крепкое, безалкогольное
+    //String brozhenie;// тип брожения: лагер, эль, ламбик, пшеничное, гибридное, особое
+    //String sirjo; // тип сырья: Ячмень, пшеница, рожь, рис, кукуруза
+
+
+    public Beer (String mark, String cvet, String strana) {
+        this.mark = mark;
+        this.cvet = cvet;
+        this.strana = strana;
+        //this.krepost = krepost;
+        //this.brozhenie = brozhenie;
+        //this.sirjo = sirjo;
+    }
+
+
+
+
+
+}
---
+++ app/src/main/java/com/hfad/findbeer101/Marks.java
@@ -0,0 +1,19 @@
+package com.hfad.findbeer101;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Marks {
+
+    public void getMarks() {
+        ArrayList <Beer> mark = new ArrayList<>();
+        mark.add(new Beer("Жигулёвское","Светлое", "Россия" ));
+        mark.add(new Beer("Колос", "Светлое", "Россия"));
+
+    }
+
+
+
+
+}
